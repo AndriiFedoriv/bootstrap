d4700270d66d2e7334dc43e2a68b3b2ba615a86a
Andrii <56352523+AndriiFedoriv@users.noreply.github.com>
2020-04-07 14:59:52 +0300

    update product-list

--- js/product-list.js
+++ js/product-list.js
@@ -53,7 +53,7 @@ class ProductList {
         )
       );
     if (currentUrlId == "sort-by-category-for-men") {
-      document.addEventListener("load", () =>
+      document.addEventListener("window", () =>
         this.sortByCategoryForMen()
       );
     }
