9d51adbb91dce55333545d6d44c99fbe7d80f7e5
Andrii <56352523+AndriiFedoriv@users.noreply.github.com>
2020-11-21 11:59:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // This is simple email sender by programming mentor
 // Usage:
 // Install deps: npm i express nodemailer body-parser
-// Run: GMAIL=fedorivandrij@gmail.com PASSWORD=123456789 node server.js
+// Run: GMAIL=secondopto@gmail.com PASSWORD=123456789 node server.js
 
 const express = require("express");
 const nodemailer = require("nodemailer");
